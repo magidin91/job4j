6a93473e010fd54e30e3ccb0adcd922b8503678a
magidin91 <magidin@mail.ru>
2019-12-24 20:28:06 +0500

    4.5. Отладка программы в IDEA[#214753]

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,14 +1,10 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
-        return Math.pow(p,2)*k/(4*Math.pow(k+1,2));
-    }
-
-    public static void main(String[] args) {
-        double result1 = square(4, 1);
-        System.out.println(" p = 4, k = 1, s = 1, real = " + result1);
-        double result2 = square(6, 2);
-        System.out.println(" p = 6, k = 2, s = 2, real = " + result2);
+    public static int square(int p, int k) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
     }
 }
