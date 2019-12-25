33d428cc02e6a7e57f1a2e52910d47352809c53e
magidin91 <magidin@mail.ru>
2019-12-26 01:21:48 +0500

    5.7. Ипотека.[#214821] ver.2

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public int year(int amount, int salary, double percent) {
         double resultAmount=amount;
-        int year=1;
-        while(resultAmount*(1+percent/100)-salary>0){
-            resultAmount=resultAmount*(1+percent/100)-salary;
+        int year=0;
+        while(resultAmount>0){
+            resultAmount = resultAmount*(1+percent/100)-salary;
             year++;
         }
         return year;
