dde94e4d2c875f45f12abb1a02fcd6cd58da80da
magidin91 <magidin@mail.ru>
2020-01-05 05:50:19 +0500

    5. Подсчет функции в диапазоне.[#217176]

---
+++ chapter_001/src/main/java/ru/job4j/other/garbage/StringFunc.java
@@ -0,0 +1,29 @@
+package ru.job4j.other.garbage;
+
+interface StringFunc {
+    String func(String n);
+}
+
+class MyStringOps {
+    static String strReverse(String str) {
+        String result = "";
+        int i;
+        for (i = str.length() - 1; i >= 0; i--)
+            result += str.charAt(i);
+        return result;
+    }
+}
+
+class MethodRefDemo {
+
+    static String stringOp(StringFunc sf, String s) {
+        return sf.func(s);
+    }
+
+    public static void main(String args[]) {
+        String inStr = "Лямбда-выражения повышают "
+                + "эффективность Java";
+        String outStr;
+        outStr = stringOp(MyStringOps::strReverse, inStr);
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/other/lyambda/EmptyArrayException.java
@@ -0,0 +1,16 @@
+package ru.job4j.other.lyambda;
+
+class EmptyArrayException extends Exception {
+    EmptyArrayException() {
+        super("Maccив пуст");
+    }
+    static void addZZZ(){
+        System.out.println("yes");
+    }
+
+    public static void main(String[] args) {
+        EmptyArrayException a= new EmptyArrayException();
+
+
+    }
+}
