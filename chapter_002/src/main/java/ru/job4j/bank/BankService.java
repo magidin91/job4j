a8023c8ac1603d6a1b5022778330871c477abbc0
magidin91 <magidin@mail.ru>
2020-01-14 19:00:41 +0500

    3. Банковские переводы. [#10038]

---
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,49 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        users.get(findByPassport(passport)).add(account);
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (passport.equals(user.getPassport()))
+                return user;
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        for (Account account : users.get(findByPassport(passport))) {
+            if (requisite.equals(account.getRequisite()))
+                return account;
+        }
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        double srcBalance = findByRequisite(srcPassport, srcRequisite).getBalance();
+        double destBalance = findByRequisite(destPassport, destRequisite).getBalance();
+        Account srcAccount = findByRequisite(srcPassport, srcRequisite);
+        Account destAccount = findByRequisite(destPassport, destRequisite);
+        if (srcAccount != null && destAccount != null && srcBalance >= amount) {
+            srcAccount.setBalance(srcBalance - amount);
+            destAccount.setBalance(destBalance + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}
