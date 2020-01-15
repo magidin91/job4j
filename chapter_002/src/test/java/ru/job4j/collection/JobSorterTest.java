40c15e7a8121b516923a87adba840e073c4d5cd5
magidin91 <magidin@mail.ru>
2020-01-15 16:05:13 +0500

    2. Сортировка User с использованием Comparator [#10036]

--- chapter_002/src/test/java/ru/job4j/collection/JobSorterTest.java
+++ chapter_002/src/test/java/ru/job4j/collection/JobSorterTest.java
@@ -26,4 +26,39 @@ public class JobSorterTest {
         Collections.sort(jobs);
         assertThat(jobs, is(expected));
     }
+
+    @Test
+    public void sortComparatorByName() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bugs", 4),
+                new Job("Impl task", 2),
+                new Job("Reboot server", 1)
+        );
+        List<Job> expected = Arrays.asList(
+                new Job("Reboot server", 1),
+                new Job("Impl task", 2),
+                new Job("Fix bugs", 4)
+        );
+        Collections.sort(jobs, new JobDescByName());
+        assertThat(jobs, is(expected));
+    }
+
+    @Test
+    public void sortComparatorByNameThanByPriority() {
+        List<Job> jobs = Arrays.asList(
+                new Job("Fix bugs", 4),
+                new Job("Impl task", 2),
+                new Job("Reboot server", 1),
+                new Job("Fix bugs", 5)
+        );
+        List<Job> expected = Arrays.asList(
+                new Job("Reboot server", 1),
+                new Job("Impl task", 2),
+                new Job("Fix bugs", 5),
+                new Job("Fix bugs", 4)
+        );
+        Collections.sort(jobs, new JobDescByName().thenComparing(new JobDescByPriority()));
+        assertThat(jobs, is(expected));
+    }
+
 }
