6252e8797edda3625f764a4d5194cf6d6ae5d75a
magidin91 <magidin@mail.ru>
2020-01-18 06:17:47 +0500

    0. Stream API улучшения[#217191]

---
+++ chapter_002/src/main/java/ru/job4j/collection/stream/api/improvements/Student.java
@@ -0,0 +1,50 @@
+package ru.job4j.collection.stream.api.improvements;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+class Student implements Comparable<Student> {
+    private String name;
+    private int scope;
+
+    public Student(String name, int scope) {
+        this.name = name;
+        this.scope = scope;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScope() {
+        return scope;
+    }
+
+    @Override
+    public int compareTo(@NotNull Student o) {
+        return Integer.compare(o.getScope(), scope);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return scope == student.scope &&
+                name.equals(student.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, scope);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", scope=" + scope +
+                '}';
+    }
+}
