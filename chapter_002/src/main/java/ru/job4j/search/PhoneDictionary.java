5f9f54b8805c3bf3e99e938072663f99b53982b7
magidin91 <magidin@mail.ru>
2020-01-18 06:52:57 +0500

    2. Local-Variable Type Inference[#217193]

--- chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_002/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -15,13 +15,12 @@ public class PhoneDictionary {
      * @return Список подощедщих пользователей.
      */
     public ArrayList<Person> find(String key) {
-        ArrayList<Person> result = new ArrayList<>();
-        for (Person person:persons) {
+        var result = new ArrayList<Person>();
+        for (var person:persons) {
           if (person.getName().contains(key) || person.getAddress().contains(key) || person.getPhone().contains(key) || person.getSurname().contains(key)) {
               result.add(person);
           }
         }
-
         return result;
     }
 }
