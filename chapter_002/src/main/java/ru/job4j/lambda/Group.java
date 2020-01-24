9f2a9d42bbb28c3e406aa826e608c730a2c5391f
magidin91 <magidin@mail.ru>
2020-01-24 16:01:04 +0500

    Группировка по интересам. [#106993]

---
+++ chapter_002/src/main/java/ru/job4j/lambda/Group.java
@@ -0,0 +1,29 @@
+package ru.job4j.lambda;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Group {
+
+    public static Map<String, Set<String>> sections(List<Student> students) {
+        // преобразуем поток Student-ов в поток Holder-ов
+        return students.stream().flatMap(student -> student.getUnits().stream().map(unit -> new Holder(unit, student.getName()))
+        ).collect( // собираем карту
+                Collectors.groupingBy(holder -> holder.key, // в качестве ключей используем назв-ия секций
+                        Collector.of( //определяем коллектор для value в виде HashSet имен учеников
+                                HashSet::new, // функция создания объекта для хранения элемента
+                                (set, el) -> set.add(el.value), // функция добавления элемента в набор
+                                (left, right) -> {
+                                    left.addAll(right);
+                                    return left;  // функции агрегации наборов;
+                                }
+                        )
+                )
+        );
+
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/lambda/GroupTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.lambda;
+
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+public class GroupTest {
+    @Test
+    public void  getSection(){
+        Map<String, Set<String>> rsl = Group.sections(List.of(new Student("Ivan",Set.of("Run","Boxing")),new Student("Andrei",Set.of("Run","Boxing", "Chess")),
+                new Student("Alexei",Set.of("Run","Chess"))));
+        assertThat(rsl.get("Chess"), is(Set.of("Andrei", "Alexei")));
+    }
+
+}
