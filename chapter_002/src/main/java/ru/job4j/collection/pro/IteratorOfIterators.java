2c564e65ceeeff4952bb08fedb120c01e3003e25
magidin91 <magidin@mail.ru>
2020-01-28 23:42:03 +0500

    5.1.4. Создать convert(Iterator<Iterator>)[#216866]

---
+++ chapter_002/src/main/java/ru/job4j/collection/pro/IteratorOfIterators.java
@@ -0,0 +1,44 @@
+package ru.job4j.collection.pro;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class IteratorOfIterators {
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            Iterator<Integer> inIterator;
+
+            {
+                if (it.hasNext()) {
+                    inIterator = it.next();
+                }
+            }
+
+            @Override
+            public boolean hasNext() {
+                boolean rsl = false;
+
+                if (it.hasNext() && !inIterator.hasNext()) {
+                    inIterator = it.next();
+                }
+
+                if (inIterator != null) { //проверяем случай, когда нет ни одного Iterator<Integer>
+                    rsl = inIterator.hasNext();
+                }
+                return rsl;
+            }
+
+            @Override
+            public Integer next() {
+                if (inIterator == null) {
+                    throw new NoSuchElementException();
+                }
+
+                if (!inIterator.hasNext()) {
+                    inIterator = it.next();
+                }
+                return inIterator.next();
+            }
+        };
+    }
+}
