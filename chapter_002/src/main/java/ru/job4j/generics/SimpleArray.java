aab85c5c186a5df97cf7bf8263ef8da15a1182e6
magidin91 <magidin@mail.ru>
2020-02-01 05:43:59 +0500

    5.2.1. Реализовать SimpleArray<T>[#216870]

---
+++ chapter_002/src/main/java/ru/job4j/generics/SimpleArray.java
@@ -0,0 +1,50 @@
+package ru.job4j.generics;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class SimpleArray<T> implements Iterable<T> {
+    private Object[] vals;
+    private int ind;
+
+    public SimpleArray(int lenghth) {
+        vals = new Object[lenghth];
+    }
+
+    public void add(T model) throws ArrayIndexOutOfBoundsException {
+        vals[ind++] = model;
+    }
+
+    public void set(int index, T model) {
+        if (index >= vals.length) {
+            System.out.println("ВЫ ввели неверный индекс");
+            return;
+        }
+        vals[index] = model;
+    }
+
+    public Object get(int index) throws ArrayIndexOutOfBoundsException {
+        return index < vals.length ? vals[index] : null;
+    }
+
+    public void remove(int index) {
+        if (index >= vals.length) {
+            System.out.println("ВЫ ввели неверный индекс");
+            return;
+        }
+        if (index != vals.length - 1) {
+            System.arraycopy(vals, index + 1, vals, index, vals.length - index - 1);
+        }
+        vals[vals.length - 1] = null;
+        ind--;
+    }
+
+    @NotNull
+    @Override
+    public Iterator<T> iterator() {
+        return (Iterator<T>) Arrays.asList(this.vals).iterator();
+    }
+}
+
---
+++ chapter_002/src/test/java/ru/job4j/generics/SimpleArrayTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.generics;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleArrayTest {
+
+    @Test
+    public void remove() {
+        SimpleArray<Integer> array = new SimpleArray<>(10);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.remove(2);
+        assertNull(array.get(2));
+    }
+
+    @Test
+    public void iterator() {
+        SimpleArray<Integer> array = new SimpleArray<>(10);
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        Iterator<Integer> it = array.iterator();
+        it.hasNext();
+        it.next();
+        assertThat(it.next(), is(2));
+    }
+}
