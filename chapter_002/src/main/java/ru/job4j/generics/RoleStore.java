3d826db00941b41c8a3826025333c9d821c01445
magidin91 <magidin@mail.ru>
2020-02-02 06:01:02 +0500

    5.2.2. Реализовать Store<T extends Base>[#216871]

---
+++ chapter_002/src/main/java/ru/job4j/generics/RoleStore.java
@@ -0,0 +1,8 @@
+package ru.job4j.generics;
+
+public class RoleStore extends AbstractStore<User> {
+
+    public RoleStore(int size) {
+        super(size);
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/generics/UserStore.java
@@ -0,0 +1,8 @@
+package ru.job4j.generics;
+
+public class UserStore extends AbstractStore<User> {
+
+    public UserStore(int size) {
+        super(size);
+    }
+}
