75404e8f76d4c1b240ab5cd9e8e9e87abe9d251c
magidin91 <magidin@mail.ru>
2020-02-06 02:35:04 +0500

    5.2.1. Реализовать SimpleArray<T>[#216870]

--- chapter_005/src/main/java/ru/job4j/collection/pro/generics/SimpleArray.java
+++ chapter_005/src/main/java/ru/job4j/collection/pro/generics/SimpleArray.java
@@ -2,8 +2,8 @@ package ru.job4j.collection.pro.generics;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class SimpleArray<T> implements Iterable<T> {
     private Object[] vals;
@@ -17,22 +17,23 @@ public class SimpleArray<T> implements Iterable<T> {
         vals[ind++] = model;
     }
 
-    public void set(int index, T model) {
+    public void set(int index, T model) throws ArrayIndexOutOfBoundsException {
         if (index >= vals.length || index < 0) {
-            System.out.println("ВЫ ввели неверный индекс");
-            return;
+            throw new ArrayIndexOutOfBoundsException("ВЫ ввели неверный индекс");
         }
         vals[index] = model;
     }
 
-    public T get(int index) {
-        return (T) (index < vals.length && index >= 0 ? vals[index] : null);
+    public T get(int index) throws ArrayIndexOutOfBoundsException {
+        if (index >= vals.length || index < 0) {
+            throw new ArrayIndexOutOfBoundsException("ВЫ ввели неверный индекс");
+        }
+        return (T) vals[index];
     }
 
-    public void remove(int index) {
+    public void remove(int index) throws ArrayIndexOutOfBoundsException {
         if (index >= vals.length || index < 0) {
-            System.out.println("ВЫ ввели неверный индекс");
-            return;
+            throw new ArrayIndexOutOfBoundsException("ВЫ ввели неверный индекс");
         }
         if (index != vals.length - 1) {
             System.arraycopy(vals, index + 1, vals, index, vals.length - index - 1);
@@ -44,7 +45,22 @@ public class SimpleArray<T> implements Iterable<T> {
     @NotNull
     @Override
     public Iterator<T> iterator() {
-        return (Iterator<T>) Arrays.asList(this.vals).iterator();
+        return new Iterator<T>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < vals.length;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (T) vals[index++];
+            }
+        };
     }
 }
 
--- chapter_005/src/test/java/ru/job4j/collection/pro/generics/SimpleArrayTest.java
+++ chapter_005/src/test/java/ru/job4j/collection/pro/generics/SimpleArrayTest.java
@@ -3,6 +3,7 @@ package ru.job4j.collection.pro.generics;
 import org.junit.Test;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
@@ -29,5 +30,70 @@ public class SimpleArrayTest {
         it.hasNext();
         it.next();
         assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void checkAdd() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(5);
+        simpleArray.add("ss");
+        simpleArray.add("bb");
+        String[] expected = new String[]{"ss", "bb"};
+        String[] actual = new String[2];
+        actual[0] = simpleArray.get(0);
+        actual[1] = simpleArray.get(1);
+        assertThat(actual, is(expected));
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenAddArrayIndexOutOfBounds3to2() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("ss");
+        simpleArray.add("bb");
+        simpleArray.add("sss");
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenAddArrayIndexOutOfBounds() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(0);
+        simpleArray.add("ss");
+        simpleArray.add("bb");
+        simpleArray.add("sss");
+    }
+
+    @Test(expected = ArrayIndexOutOfBoundsException.class)
+    public void whenArrayIndexOutOfBounds() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(5);
+        simpleArray.set(5, "ss");
+    }
+
+    @Test
+    public void whenAddARemoveOne() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("ss");
+        simpleArray.remove(0);
+        assertNull(simpleArray.get(0));
+    }
+
+    @Test
+    public void whenAddRemoveOne() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(5);
+        simpleArray.add("ss");
+        simpleArray.add("aa");
+        simpleArray.add("ff");
+        simpleArray.remove(1);
+        String expected = "rr";
+        simpleArray.add(expected);
+        assertThat(simpleArray.get(2), is(expected));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void iterator3Next2lenghth() {
+        SimpleArray<String> simpleArray = new SimpleArray<>(2);
+        simpleArray.add("ss");
+        simpleArray.add("bb");
+        Iterator<String> iterator = simpleArray.iterator();
+        iterator.next();
+        iterator.next();
+        iterator.next();
     }
 }
