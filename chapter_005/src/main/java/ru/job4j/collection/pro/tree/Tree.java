b1272e3865caacee7d53e9e1cb2c606ab0c82f11
magidin91 <magidin@mail.ru>
2020-02-19 01:23:50 +0500

    2. Добавить метод boolean isBinary()

--- chapter_005/src/main/java/ru/job4j/collection/pro/tree/Tree.java
+++ chapter_005/src/main/java/ru/job4j/collection/pro/tree/Tree.java
@@ -2,8 +2,9 @@ package ru.job4j.collection.pro.tree;
 
 import java.util.*;
 
-class Tree<E> implements SimpleTree<E> {
+class Tree<E extends Comparable<E>> implements SimpleTree<E> {
     private final Node<E> root;
+    private boolean isBinary = true;
 
     Tree(final E root) {
         this.root = new Node<>(root);
@@ -25,6 +26,21 @@ class Tree<E> implements SimpleTree<E> {
             rsl = true;
         }
         return rsl;
+    }
+
+    public boolean isBinary() {
+        recursion(root);
+        return isBinary;
+    }
+
+    private void recursion(Node<E> node) {
+        if (node.children.size() > 2) {
+            isBinary = false;
+            return;
+        }
+        for (Node<E> element : node.children) {
+            recursion(element);
+        }
     }
 
     @Override
