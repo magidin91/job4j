4a5d37a8ef68679d4f64efd0cd58e7739ba318c2
magidin91 <magidin@mail.ru>
2020-02-18 19:13:01 +0500

    8. Реализовать собственную структуру данных - HashMap[#216884]

---
+++ chapter_005/src/main/java/ru/job4j/collection/pro/map/SimpleHashMap.java
@@ -0,0 +1,141 @@
+package ru.job4j.collection.pro.map;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class SimpleHashMap<K, V> implements Iterable<SimpleHashMap.Node<K, V>> {
+    private static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;
+    private Node<K, V>[] tab;
+    private int size;
+    private int n; //длина массива
+    private final float LOAD_FACTOR = 0.75f;
+    private int modCount;
+    /**
+     * Количество элементов, при котором удваиваем длину массива
+     */
+    int threshold;
+
+    public SimpleHashMap() {
+        this.tab = new Node[DEFAULT_INITIAL_CAPACITY];
+        n = DEFAULT_INITIAL_CAPACITY;
+        threshold = (int) (n * LOAD_FACTOR);
+    }
+
+    /**
+     * @param capacity должен быть 2^n.
+     */
+    public SimpleHashMap(int capacity) {
+        this.tab = new Node[capacity];
+        n = capacity;
+        threshold = (int) (n * LOAD_FACTOR);
+    }
+
+    public boolean insert(K key, V value) {
+        int hash = hash(key);
+        resize();
+        int i = forIndex(hash);
+
+        if (tab[i] == null) {
+            Node<K, V> node = new Node<K, V>(key, value, hash);
+            tab[i] = node;
+            size++;
+            modCount++;
+            return true;
+        }
+        return false;
+    }
+
+    public V get(K key) {
+        int i = forIndex(hash(key));
+        V value = null;
+        Node<K, V> node = tab[i];
+        if (node != null) {
+            if ((key == null && node.key == null) || (key != null && key.equals(node.key))) { //проверяем тот ли ключ, лежит в ноде из ячейки
+                value = node.value;
+            }
+        }
+        return value;
+    }
+
+    public boolean delete(K key) {
+        boolean rsl = false;
+        int i = forIndex(hash(key));
+        Node<K, V> node = tab[i];
+        if (node != null) {
+            if ((key == null && node.key == null) || (key != null && key.equals(node.key))) {
+                tab[i] = null;
+                rsl = true;
+                size--;
+                modCount++;
+            }
+        }
+        return rsl;
+    }
+
+    private void resize() {
+        if (size == threshold) {
+            Node<K, V>[] oldTab = tab;
+            tab = new Node[n << 1];
+            n = n << 1;
+            threshold = threshold << 1;
+            for (Node node : oldTab) {
+                if (node != null) {
+                    tab[forIndex(node.hash)] = node;
+                }
+            }
+        }
+    }
+
+    private int forIndex(int hash) {
+        return (n - 1) & hash;
+    }
+
+    static final int hash(Object key) {
+        int h;
+        return (key == null) ? 0 : (h = key.hashCode()) ^ (h >>> 16);
+    }
+
+    @NotNull
+    @Override
+    public Iterator<Node<K, V>> iterator() {
+        return new Iterator<>() {
+            int expectedModCount = modCount;
+            private int cursor;
+
+            @Override
+            public boolean hasNext() {
+                return cursor != n;
+            }
+
+            @Override
+            public Node<K, V> next() throws NoSuchElementException, ConcurrentModificationException {
+                checkForComodification();
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return tab[cursor++];
+            }
+
+            private final void checkForComodification() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+            }
+        };
+    }
+
+    static class Node<K, V> {
+        final K key;
+        V value;
+        final int hash;
+
+        Node(K key, V value, int hash) {
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+        }
+    }
+}
---
+++ chapter_005/src/test/java/ru/job4j/collection/pro/map/SimpleHashMapTest.java
@@ -0,0 +1,83 @@
+package ru.job4j.collection.pro.map;
+
+import org.junit.Test;
+
+import java.util.ConcurrentModificationException;
+import java.util.NoSuchElementException;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class SimpleHashMapTest {
+
+
+    @Test
+    public void insert2Equal2Get1() {
+        SimpleHashMap<String, Integer> simpleHashMap = new SimpleHashMap<>();
+        simpleHashMap.insert("a", 100);
+        simpleHashMap.insert("a", 200);
+        assertThat(simpleHashMap.get("a"), is(100));
+    }
+
+    @Test
+    public void insert2Get2() {
+        SimpleHashMap<String, Integer> simpleHashMap = new SimpleHashMap<>();
+        simpleHashMap.insert("a", 100);
+        simpleHashMap.insert("b", 200);
+        assertThat(simpleHashMap.get("a"), is(100));
+        assertThat(simpleHashMap.get("b"), is(200));
+    }
+
+    @Test
+    public void insertAndResize() {
+        SimpleHashMap<String, Integer> simpleHashMap = new SimpleHashMap<>();
+        for (int i = 0; i != 100; i++) {
+            String key = "a" + i;
+            simpleHashMap.insert(key, i);
+        }
+        assertThat(simpleHashMap.get("a18"), is(18));
+    }
+
+    @Test
+    public void insert2Delete2() {
+        SimpleHashMap<String, Integer> simpleHashMap = new SimpleHashMap<>();
+        simpleHashMap.insert("a", 100);
+        simpleHashMap.insert("b", 200);
+        simpleHashMap.insert("c", 300);
+        assertThat(simpleHashMap.get("b"), is(200));
+        assertThat(simpleHashMap.get("c"), is(300));
+        simpleHashMap.delete("b");
+        simpleHashMap.delete("c");
+        assertNull(simpleHashMap.get("b"));
+        assertNull(simpleHashMap.get("c"));
+    }
+
+    @Test
+    public void iterator16TrueLastFalse() {
+        SimpleHashMap<String, Integer> simpleHashMap = new SimpleHashMap<>();
+        var iterator = simpleHashMap.iterator();
+        for (int i = 0; i != 16; i++) {
+            assertThat(iterator.hasNext(), is(true));
+            iterator.next();
+        }
+        assertThat(iterator.hasNext(), is(false));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void NoSuchElementException() {
+        SimpleHashMap<String, Integer> simpleHashMap = new SimpleHashMap<>();
+        var iterator = simpleHashMap.iterator();
+        for (int i = 0; i != 16; i++) {
+            iterator.next();
+        }
+        iterator.next();
+    }
+
+    @Test(expected = ConcurrentModificationException.class)
+    public void iteratorModificationException() {
+        SimpleHashMap<String, Integer> simpleHashMap = new SimpleHashMap<>();
+        var iterator = simpleHashMap.iterator();
+        simpleHashMap.insert("c", 300);
+        iterator.next();
+    }
+}
