042a9c876526bd078a4e624f4b1f4a3e786adaf5
magidin91 <magidin@mail.ru>
2020-04-28 05:35:58 +0500

    1. Исправить код класс SimpleGenerator.2

--- chapter_008/src/test/java/ru/job4j/design/srp/tdd/GeneratorTest.java
+++ chapter_008/src/test/java/ru/job4j/design/srp/tdd/GeneratorTest.java
@@ -10,29 +10,35 @@ import static org.junit.Assert.*;
 
 @Ignore
 public class GeneratorTest {
-    private final Generator generator = (template, args) -> null;
-    private final String template = "I am a ${name}, Who are ${subject}?";
 
     @Test(expected = IllegalStateException.class)
     public void getTemplateKeysMoreMapKeys() throws Exception {
+        Generator generator = (template, args) -> null;
+        String template = "I am a ${name}, Who are ${subject}?";
         Map<String, String> args = Map.of("name", "John");
         generator.produce(template, args);
     }
 
     @Test(expected = IllegalStateException.class)
     public void getTemplateKeysMoreAppropriateMapKeys() throws Exception {
+        Generator generator = (template, args) -> null;
+        String template = "I am a ${name}, Who are ${subject}?";
         Map<String, String> args = Map.of("name", "John", "OBJECT", "you");
         generator.produce(template, args);
     }
 
     @Test(expected = IllegalStateException.class)
     public void getMapExtraKeys() throws Exception {
+        Generator generator = (template, args) -> null;
+        String template = "I am a ${name}, Who are ${subject}?";
         Map<String, String> args = Map.of("name", "John", "subject", "you", "name2", "John2");
         generator.produce(template, args);
     }
 
     @Test
     public void getAppropriateKeys() throws Exception {
+        Generator generator = (template, args) -> null;
+        String template = "I am a ${name}, Who are ${subject}?";
         Map<String, String> args = Map.of("name", "John", "subject", "you");
         String exp = "I am a John, Who are you?";
         assertThat(generator.produce(template, args), is(exp));
