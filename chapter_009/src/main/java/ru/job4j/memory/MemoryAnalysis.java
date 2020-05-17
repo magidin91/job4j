c7448a95bed2c0cdd10b044b24cded7f6c1dc8fb
magidin91 <magidin@mail.ru>
2020-05-18 04:17:03 +0500

    1. Демонстрация работы GC[#216933]

---
+++ chapter_009/src/main/java/ru/job4j/memory/MemoryAnalysis.java
@@ -0,0 +1,24 @@
+package ru.job4j.memory;
+
+public class MemoryAnalysis {
+
+    public void create(int number) {
+        info();
+        for (int i = 0; i < number; i++) {
+            new User(String.valueOf(i), String.valueOf(2 * i), i);
+        }
+    }
+
+    public void info() {
+        int mb = 1024 * 1024;
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println(String.format("usedMemory: %d", (runtime.totalMemory() - runtime.freeMemory()) / mb));
+        System.out.println(String.format("freeMemory: %d", runtime.freeMemory() / mb));
+        System.out.println(String.format("totalMemory: %d", runtime.totalMemory() / mb));
+        System.out.println(String.format("maxMemory: %d", runtime.maxMemory() / mb));
+    }
+
+    public static void main(String[] args) {
+        new MemoryAnalysis().create(2000);
+    }
+}
