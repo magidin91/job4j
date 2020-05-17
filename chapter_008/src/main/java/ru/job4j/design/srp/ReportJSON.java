73ad6ffe18417427a73ede22a5f7eca19e63ce74
magidin91 <magidin@mail.ru>
2020-05-17 20:02:08 +0500

    2. OCP. На базе задания из занятия SRP добавить поддержку форматом XML, JSON в генераторе отчетов.[#216916]

---
+++ chapter_008/src/main/java/ru/job4j/design/srp/ReportJSON.java
@@ -0,0 +1,26 @@
+package ru.job4j.design.srp;
+
+import java.util.StringJoiner;
+import java.util.function.Predicate;
+
+public class ReportJSON {
+    private static final String LN = System.lineSeparator();
+    private Store store;
+
+    public ReportJSON(Store store) {
+        this.store = store;
+    }
+
+    public String generate(Predicate<Employee> filter) {
+        StringJoiner json = new StringJoiner(LN,
+                "{" + LN,
+                LN + "}");
+        for (Employee employee : store.findBy(filter)) {
+            json.add("\"name\": \"" + employee.getName() + "\"")
+                    .add("\"hired\": \"" + employee.getHired() + "\"")
+                    .add("\"fired\": \"" + employee.getFired() + "\"")
+                    .add("\"salary\": " + employee.getSalary());
+        }
+        return json.toString();
+    }
+}
